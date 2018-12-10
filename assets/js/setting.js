e02cb7d0be833b2bdd85c4f23abbeb21eac02fe1
Gladibeth <gladibethdelos@gmail>
2018-12-10 11:55:08 -0400

    first commit

---
+++ assets/js/setting.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".single-item").slick({
+        autoplay: false,
+        infinite: true,
+        cssEase: ' ease-in-out',
+        fade: true,
+        arrows: false,
+        dots: false,
+        speed: 900,
+        infinite: true,
+        cssEase: 'cubic-bezier(0.420, 0.000, 0.580, 1.000)'
+    });
+});
+
+// transition: all 500ms; /* ease-in-out */
